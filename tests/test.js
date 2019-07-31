81222cce20779b617415115becec83e5ff6d39c8
georgia.shaw <georgia@cranbrookw4.com>
2019-07-31 16:06:03 +0100

    first proper test using supertest we have written a test that mimics the server to check that the server is working correctly

--- tests/test.js
+++ tests/test.js
@@ -7,3 +7,15 @@ test('initialise', (t) =>{
   t.equal(number , 2 , 'Should return 2');
   t.end();
 })
+
+test('status code is 200', (t) => {
+  supertest(router)
+  .get('/')
+  .expect(200)
+  .expect('Content-type', /html/)
+  .end((err, res) => {
+    t.error(err);
+    t.equal(res.statusCode, 200, 'should return 200')
+    t.end();
+  })
+});
