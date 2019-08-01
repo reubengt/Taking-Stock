09bd4691afd021365c861fd8afe0ce671a9e1f1f
reubengt <reubengeorgethomas@gmail.com>
2019-08-01 12:43:13 +0100

    add request module for back-end

---
+++ src/request.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const myRequest = (url, cb) => {
+  http.get(url, (res) => {
+    let statusCode = res.statusCode
+    let body='';
+    res.on('data', (chunk) => {
+      body+=chunk;
+    })
+    res.on('end', () => {
+      let responseObj = {};
+      responseObj.body=JSON.parse(body);
+      responseObj.statusCode=statusCode;
+      if(statusCode !== 200)
+      {
+       cb(new TypeError("StatusCode is not 200, request failed"))
+      }
+      else
+      {
+        cb(null, responseObj)
+      }
+    })
+  })
+}
