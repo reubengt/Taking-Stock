1f624354591fce81bfde8d790663797a26b066f1
frannyfra <francescadelaurentis1987@gmail.com>
2019-08-02 12:18:45 +0100

    deleting console.log

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ let response;
 const query = e => {
   let searchValue = document.querySelector(".dropdown").value; //search query value taken from select dropdown
   let queryUrl = `/search?q=${searchValue}`;
-  console.log(searchValue);
+  
   //clear price container
   let priceContainer = document.querySelector('.price-section');
   while (priceContainer.firstChild) {
@@ -16,12 +16,11 @@ const query = e => {
 
   xhr.onreadystatechange = () => {
     if(xhr.readyState === 4 && xhr.status === 200) {
-     response = JSON.parse(xhr.responseText);
-     console.log(response, 'hello')
-     let span = document.createElement('span');
-    span.textContent = response;
-    span.classList.add('price-span');
-    priceContainer.appendChild(span);
+      response = JSON.parse(xhr.responseText);
+      let span = document.createElement('span');
+      span.textContent = response;
+      span.classList.add('price-span');
+      priceContainer.appendChild(span);
     }
   }
   xhr.open("GET", queryUrl, true);
